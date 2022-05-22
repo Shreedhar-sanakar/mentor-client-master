850192e099c2dea5c0be85e8d6425181f424749d
Shreedhar-sanakar <shridhar.sanakar@gmail.com>
2022-05-23 00:09:05 +0530

    first commit

---
+++ src/Context/AssignMentors.js
@@ -0,0 +1,37 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+export const AssignMentorsContext = React.createContext();
+
+export const AssignMentorProvider = ({ children }) => {
+  const [students, setStudents] = useState([]);
+  const [mentors, setMentors] = useState([]);
+  const BaseURL = `https://assign-students-mentor.herokuapp.com/`;
+  const fetchData = async () => {
+    await axios
+      .get(`${BaseURL}/Mentors`)
+      .then((response) => setMentors(response.data))
+      .then(() => console.log(mentors));
+
+    await axios
+      .get(`${BaseURL}/Students`)
+      .then((response) => setStudents(response.data))
+      .then(() => console.log(students));
+  };
+  useEffect(() => {
+    fetchData();
+    return () => {
+      <></>;
+    };
+  }, []);
+  return (
+    <>
+      {console.log(mentors, students)}
+      <AssignMentorsContext.Provider
+        value={[mentors, setMentors, students, setStudents]}
+      >
+        {children}
+      </AssignMentorsContext.Provider>
+    </>
+  );
+};
